58252ce1c99752a93686575d9b1eb1d91f4ee82c
Zinath22 <Zinath22@users.noreply.github.com>
2024-01-15 20:38:45 +0600

    some change

--- src/Pages/Donation/DonationCard.jsx
+++ src/Pages/Donation/DonationCard.jsx
@@ -16,19 +16,17 @@ const DonationCard = ({ donate }) => {
   } = donate || {};
 
   return (
-  
-      
-      <div style={{background:card_bg}} className="flex rounded-lg flex-col md:flex-row gap-4 items-center ">
-        <img className="" src={card_img} alt={title} />
-        <div>
-          <h3 className=" py-1 px-2 mb-2 rounded inline-block font-semibold text-lg text-center" style={{ color:text_color, backgroundColor:category_name_bg}}>{category_name}</h3>
-          <p className="text-2xl text-black mb-2 font-semibold">{title}</p>
-          <p className="text-lg font-semibold" style={{color: text_color}}>${price}</p>
-          <Link to={`/donations/${id}`}>
-          <button className="btn text-lg text-white p-2 rounded mt-2 mb-3 " style={{background: text_color}}>View Details</button>
-          </Link>
-        </div>
+    <div style={{ background: card_bg }} className="flex rounded-lg flex-col md:flex-row gap-4 items-center ">
+      <img className="" src={card_img} alt={title} />
+      <div>
+        <h3 className=" py-1 px-2 mb-2 rounded inline-block font-semibold text-lg text-center" style={{ color: text_color, backgroundColor: category_name_bg }}>{category_name}</h3>
+        <p className="text-2xl text-black mb-2 font-semibold">{title}</p>
+        <p className="text-lg font-semibold" style={{ color: text_color }}>${price}</p>
+        <Link to={`/donations/${id}`}>
+          <button className="btn text-lg text-white p-2 rounded mt-2 mb-3 " style={{ background: text_color }}>View Details</button>
+        </Link>
       </div>
+    </div>
   );
 };
 DonationCard.propTypes = {
